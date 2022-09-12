9b7728f73f03a9bcd3a3d177998fceea56f6bdb8
blessfulsumi <blessfulsumi@gmail.com>
2022-09-12 13:14:43 +0530

    demo commit

---
+++ basics/src/main/java/mavjnit5/basics/addition.java
@@ -0,0 +1,22 @@
+package mavjnit5.basics;
+
+public class addition {
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+	public int sub(int a,int b)
+	{
+		return a-b;
+	}
+	public int mul(int a,int b)
+	{
+		return a*b;
+	}
+	public int div(int a,int b)
+	{
+		return a/b;
+	}
+
+		
+}
